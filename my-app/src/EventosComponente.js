5cd0930dc33bb04cd19bca3883292773c15ae567
alan0112 <alanmartinez01121996@gmail.com>
2024-10-19 16:11:50 -0600

    Eventos

--- my-app/src/EventosComponente.js
+++ my-app/src/EventosComponente.js
@@ -2,20 +2,65 @@ import React from 'react'
 
 export const EventosComponente = () => {
 
-    function hasDadoClick(){
-        alert("Has dado click");
+    const hasDadoClick = (e, nombre) => {
+        alert("Has dado click" + nombre);
     }
+
+    function hadDadoDobleClick() {
+        alert("Has dado doble click");
+    }
+
+    function hasEntrado(){
+        alert("Ha entrado");
+    }
+
+    const hasSalido = () =>{
+        alert("Has salido");
+    }
+
+    const introduceNombre = () =>{
+      alert("Introduce tu nombre");
+    }
+
+    const estasFuera = (e) =>{
+      alert("Estas fuera");
+    }
+    
 
   return (
     <div>
         <h1>Eventos en react</h1>
-        <button onClick={hasDadoClick}>Dame click</button>
+
+        <button onClick={e => hasDadoClick(e, " Alan")}>Has click</button>
+
+        <br/> 
+        {/*Evento doble click */ }
+        <button onDoubleClick={hadDadoDobleClick}>Dame doble click</button>
+
+        <br/>
+
+
         {/*Evento click
         <button onClick={()=>{
             console.log("Esto es un boton con eveto click");
             
 
         }}>Dame click </button>*/}
+
+        <div id= "caja" 
+        onMouseEnter={hasEntrado}
+        onMouseLeave={hasSalido}>
+          {/*Evento on mouse enter on mouse leave*/}
+          Pasa por encima
+
+        </div>
+
+        <p>
+          <input type='text' 
+          onFocus={introduceNombre} 
+          onBlur={estasFuera}
+          placeholder='Introduce tu nombre'/>
+        </p>
     </div>
   )
 }
