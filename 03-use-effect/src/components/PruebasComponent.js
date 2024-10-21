3bf60e6b74c60d704f7df2d7df404411317cc13f
alan0112 <alanmartinez01121996@gmail.com>
2024-10-20 22:47:00 -0600

    Estado use effect

---
+++ 03-use-effect/src/components/PruebasComponent.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+
+export const PruebasComponent = () => {
+
+    const [usuario, setUsuario]= useState("Alan");
+    const [fecha, setFecha]= useState("01-01-2001");
+    const [contador,setContador] =useState(0);
+
+    const modUsuario = (e) =>{
+        setUsuario(e.target.value);
+        
+    }
+
+    const cambiarFecha = (e) =>{
+        setFecha(new Date().toLocaleDateString())
+
+
+    }
+
+   
+    //Con los corchetes solo se ejecuta una vez
+    useEffect (()=>{
+        console.log("Has cargado el componente");
+        
+    },[]);
+
+     //Con los corchetes solo se ejecuta una vez
+     useEffect (()=>{
+        
+        
+        setContador(contador+1);
+        // eslint-disable-next-line no-useless-concat
+        console.log("Has cargado el componente" + "" +contador);
+    },[ usuario]);
+
+
+  return (
+    <div>
+        <h1>Pruebas Efecct</h1>
+        <strong className={contador>=10 ? 'label label1' : 'label'}>{usuario}</strong>
+        <br/>
+        <strong className='label1'>{fecha}</strong>
+        <p>
+        <input type='text' 
+        onChange={modUsuario}
+        placeholder='Cambia nombre'/>
+        <button onClick={cambiarFecha}>Cambia fecha</button>
+        </p>
+        
+    </div>
+  )
+}
